6b68e5ba9c45b1d2106b3a1ec4c1c75293f88f18
praveendnkeshava <140802574+praveendnkeshava@users.noreply.github.com>
2026-01-18 07:40:12 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { BLOG_POSTS } from '@/constants'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sfai.netlify.app'
+  const baseUrl = 'https://incredible-syrniki-9cc7aa.netlify.app'
   
   const routes = [
     '',
